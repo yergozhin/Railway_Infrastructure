a926bcab8a4eec453aecb9d7722708e09fa262d2
yergozhin <101567490+yergozhin@users.noreply.github.com>
2023-08-26 00:23:14 +0600

    Add files via upload

---
+++ MyQueueForLists.java
@@ -0,0 +1,25 @@
+public class MyQueueForLists {
+    private static class Node {
+        PairForListAndLoco pair;
+        MyQueueForLists.Node next = null;
+        Node(PairForListAndLoco d) { pair = d; }
+    }
+    private MyQueueForLists.Node head, tail;
+    public MyQueueForLists() {
+        head = tail = null;
+    }
+    public void enqueue(PairForListAndLoco s) {
+        if (head == null)
+            head = tail = new MyQueueForLists.Node(s);
+        else
+            tail = tail.next = new MyQueueForLists.Node(s);
+    }
+    public PairForListAndLoco dequeue() {
+        PairForListAndLoco s = head.pair;
+        if ((head = head.next) == null) tail = null;
+        return s;
+    }
+    public boolean empty() {
+        return head == null;
+    }
+}
