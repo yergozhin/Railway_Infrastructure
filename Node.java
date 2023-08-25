45bcdba8b2daaac7239baecf77cf72b4dffc819c
yergozhin <101567490+yergozhin@users.noreply.github.com>
2023-08-26 00:23:33 +0600

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    RailroadCar railroadCar;
+    Node next;
+    Node(RailroadCar railroadCar, Node next) {
+        this.railroadCar = railroadCar;
+        this.next = next;
+    }
+    Node(RailroadCar railroadCar) {
+        this(railroadCar,null);
+    }
+}
