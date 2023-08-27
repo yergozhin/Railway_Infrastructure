cb2d3214f5f476e4b52afa7a0c47ff2707cee6df
yergozhin <101567490+yergozhin@users.noreply.github.com>
2023-08-27 16:08:50 +0600

    Add files via upload

---
+++ PairForListAndLoco.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class PairForListAndLoco {
+    Trainset trainset;
+    List<Station> list;
+    int i;
+    boolean forward;
+    double distance;
+    public PairForListAndLoco(Trainset trainset, List<Station> list, int i, boolean forward,double distance){
+        this.trainset = trainset;
+        this.list = list;
+        this.i = i;
+        this.forward = forward;
+        this.distance = distance;
+    }
+}
