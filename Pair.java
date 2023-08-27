15f3c90c84008edc82ccdd36acf9df162cf0ac6c
yergozhin <101567490+yergozhin@users.noreply.github.com>
2023-08-27 16:08:17 +0600

    Add files via upload

---
+++ Pair.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Pair{
+    Station station;
+    List<Station> list;
+    public Pair(Station station, List<Station> list){
+        this.station = station;
+        this.list = list;
+    }
+}
