f70577790f4d3e693c30d5dc93f8fce5f3ba8e2e
yergozhin <101567490+yergozhin@users.noreply.github.com>
2023-08-28 18:19:05 +0600

    Add files via upload

---
+++ RailroadCar.java
@@ -0,0 +1,14 @@
+public class RailroadCar {
+    ElectricalGrid electricalGrid;
+    static int count = 0;
+    int weight;
+    //String name = "Common";
+    int ID;
+    public RailroadCar(){
+        count++;
+        this.ID = count;
+        weight = 0;
+        this.electricalGrid = new ElectricalGrid(false);
+        //this.name = "Common";
+    }
+}
