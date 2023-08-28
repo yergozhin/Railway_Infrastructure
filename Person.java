2e57380c6ad3d9efb9c3b17da421da4e552a1f5d
yergozhin <101567490+yergozhin@users.noreply.github.com>
2023-08-28 18:18:31 +0600

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,10 @@
+public class Person {
+    String name;
+    static int count = 0;
+    int ID;
+    public Person(String name){
+        this.name = name;
+        this.ID = count;
+        this.count++;
+    }
+}
