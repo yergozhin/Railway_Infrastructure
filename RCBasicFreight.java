e4062c1d13f980867c2d4af549dacdc9ab3b3eeb
yergozhin <101567490+yergozhin@users.noreply.github.com>
2023-08-30 00:34:18 +0600

    Add files via upload

---
+++ RCBasicFreight.java
@@ -0,0 +1,19 @@
+public abstract class RCBasicFreight extends RailroadCar{
+    double maxTotalWeight;
+    double maxTotalVolume;
+    ElectricalGrid electricalGrid;
+    String name;
+    int ID;
+    public RCBasicFreight(double maxTotalWeight, double maxTotalVolume){
+        super();
+        this.ID = ID;
+        this.maxTotalWeight = maxTotalWeight;
+        this.maxTotalVolume = maxTotalVolume;
+        this.electricalGrid = new ElectricalGrid(false);
+        this.name = "Basic Freight";
+    }
+    public abstract void addSome(double TotalWeight, double TotalVolume);
+    public abstract void removeSome(double TotalWeight, double TotalVolume);
+    public abstract void removeAll();
+
+}
