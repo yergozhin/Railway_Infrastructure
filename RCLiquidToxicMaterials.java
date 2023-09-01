5bf6563ffc66d69b4e2408d5e99b4b92a08a79ab
yergozhin <101567490+yergozhin@users.noreply.github.com>
2023-09-01 17:29:23 +0600

    Add files via upload

---
+++ RCLiquidToxicMaterials.java
@@ -0,0 +1,50 @@
+public class RCLiquidToxicMaterials extends RCHeavyFreight{
+    ElectricalGrid electricalGrid;
+    double curTotalVolume;
+    double curTotalWeight;
+    String securityInfo;
+    String name = "Liquid Toxic Materials";
+    int ID;
+    public RCLiquidToxicMaterials(double maxTotalWeight, double maxTotalVolume,String securityInfo){
+        super(maxTotalWeight,maxTotalVolume,securityInfo);
+        this.ID = ID;
+        this.curTotalVolume = 0;
+        this.curTotalWeight = 0;
+        this.electricalGrid = new ElectricalGrid(false);
+        this.name = "Liquid Toxic Materials";
+    }
+    @Override
+    public void addAtOnce(double TotalWeight, double TotalVolume) {
+        if(curTotalWeight > 0 || curTotalVolume > 0){
+            try {
+                throw new RuntimeException("You cannot add more Liquid Toxic Materials");
+            }catch (RuntimeException e){
+                System.err.println("You cannot add more Liquid Toxic Materials");
+            }
+        }
+        else if(TotalWeight > maxTotalWeight){
+            try {
+                throw new RuntimeException("Too much weight of Liquid Toxic Materials");
+            }catch (RuntimeException e){
+                System.err.println("Too much weight of Liquid Toxic Materials");
+            }
+        }else if(TotalVolume > maxTotalVolume){
+            try {
+                throw new RuntimeException("Not enough space for Liquid Toxic Materials");
+            }catch (RuntimeException e){
+                System.err.println("Not enough space for Liquid Toxic Materials");
+            }
+        }else{
+            this.curTotalWeight = TotalWeight;
+            this.curTotalVolume = TotalVolume;
+            System.out.println("Added successfully");
+        }
+    }
+
+    @Override
+    public void removeAtOnce() {
+        this.curTotalWeight = 0;
+        this.curTotalVolume = 0;
+        System.out.println("Removed successfully");
+    }
+}
