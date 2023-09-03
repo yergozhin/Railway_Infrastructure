c61149805e886e2a3a807cfad721ca6a6bbc53d6
yergozhin <101567490+yergozhin@users.noreply.github.com>
2023-09-03 12:59:08 +0600

    Add files via upload

---
+++ Station.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Station {
+    String name;
+    static int index = 0;
+    int ID;
+    static List<Station> stations = new ArrayList<>();
+    public Station(String name){
+        this.name = name;
+        this.ID = index;
+        index++;
+    };
+}
