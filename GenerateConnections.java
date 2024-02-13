5da13c237a53b0c07ccbb10d91941b635110d3f4
yergozhin <101567490+yergozhin@users.noreply.github.com>
2024-02-14 00:47:19 +0100

    Update GenerateConnections.java

--- GenerateConnections.java
+++ GenerateConnections.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import java.util.List; 
 
 public class GenerateConnections {
     public GenerateConnections(int numberOfConnectionsForEachStation, List<Station> station, Connections connections){
