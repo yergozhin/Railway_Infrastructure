d91ff0234521fbf64ba5c71802873e668874127e
yergozhin <101567490+yergozhin@users.noreply.github.com>
2024-02-27 22:41:26 +0100

    Update Run.java

--- Run.java
+++ Run.java
@@ -1,7 +1,7 @@
 import java.io.Console;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.List; 
 import java.util.Scanner;
 
 public class Run extends Thread{
